0b9d23503467be1a7282927b65895c3e17d63657
Oscar Camplin <oscar.camplin@airnz.co.nz>
2024-12-18 16:10:37 +1300

    add role arn

--- main.tf
+++ main.tf
@@ -5,49 +5,10 @@ provider "aws" {
   token      = var.SESSION_TOKEN
 }
 
-# Create an IAM role for the Lambda function
-resource "aws_iam_role" "lambda_role" {
-  name = "lambda-basic-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement: [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-# Attach a policy to the Lambda role
-resource "aws_iam_role_policy" "lambda_policy" {
-  name = "lambda-basic-policy"
-  role = aws_iam_role.lambda_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement: [
-      {
-        Effect   = "Allow",
-        Action   = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        Resource = "arn:aws:logs:*:*:*"
-      }
-    ]
-  })
-}
-
 # Create the Lambda function
 resource "aws_lambda_function" "this" {
   function_name = var.lambda_name
-  role          = aws_iam_role.lambda_role.arn
+  role          = "arn:aws:iam::146609405994:role/lambda-basic-role"
   handler       = "lambda_function.lambda_handler" # Update based on your function
   runtime       = "python3.9"                     # Adjust for your language
   timeout       = 10
